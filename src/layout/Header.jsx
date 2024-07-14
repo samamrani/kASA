09cd6c57a3d29c7ee54a68c1ea41d69625ee37c9
atmani samira <erp.atmanis@gmail.com>
2024-07-14 11:29:02 +0200

    Remplace les utilisations de Link par NavLink

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../assets/logo/logo.png'
+import { NavLink } from 'react-router-dom'; 
+import logo from '../assets/logo/logo.png';
 import '../styles/layout/_header.scss'; 
 
 function Header() {
   return (
     <header>
-    <nav>
-
-      <div className="logo">
-        <Link to="/">
-          <img src={logo} alt="Logo du site Kasa" />
-        </Link>
-      </div>
-
-      <ul className="nav-links">
-        <li><Link to="/">Accueil</Link></li>
-        <li><Link to="/about">À propos</Link></li>
-      </ul>
-     
-    </nav>
-  </header>
+      <nav>
+        <div className="logo">
+          <NavLink exact to="/" activeClassName="active">
+            <img src={logo} alt="Logo du site Kasa" />
+          </NavLink>
+        </div>
+        <div className="nav-links-container">
+          <NavLink exact className="nav-links" to="/" activeClassName="active">
+            Accueil
+          </NavLink>
+          <NavLink className="nav-links" to="/about" activeClassName="active">
+            À Propos
+          </NavLink>
+        </div>
+      </nav>
+    </header>
   );
 }
 
